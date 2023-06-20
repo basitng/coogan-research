5ccf5ed2235d78b77c2341368d6daa94ce65c200
basitng <basitng2004@gmail.com>
2023-06-20 10:46:40 +0100

    added a url host

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:9000",
     "https://coogan-research.onrender.com",
-    "coogan-research.onrender.com"
+    "coogan-research.onrender.com",
 ]
 
 
