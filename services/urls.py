71b03581c3983bb71a1cfee8f83737b5639a77c8
basitng <basitng2004@gmail.com>
2023-06-30 21:53:20 +0100

    modified changes

--- services/urls.py
+++ services/urls.py
@@ -4,5 +4,5 @@ from .views import GetVideo, VideoUploadView
 
 urlpatterns = [
     path('video/', VideoUploadView.as_view(), name='audio-file'),
-    path('vid/', GetVideo.as_view(), name=''),
+    path('', GetVideo.as_view(), name=''),
 ]
--- utils/create_file.py
+++ utils/create_file.py
@@ -1,6 +1,3 @@
-import PyPDF2
-
-
 def create_file(content, filename):
     file = open(filename, "w")
     file.write(content)
