50aa1c432f716dd838612040ee05ebaf09fe54f3
basitng <basitng2004@gmail.com>
2023-07-03 02:24:45 +0100

    cleaned up code

--- utils/openai.py
+++ utils/openai.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import openai
+import re
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -9,7 +10,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def generate_prompt(prompt):
     response = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=f"Generate at least 15 astonishing mid-journey images from the following content:{prompt}",
+        prompt=f"Generate at least 2 astonishing mid-journey images from the following content:{prompt}",
         max_tokens=500,  # Adjust the max_tokens value as needed
         temperature=0.98,
         top_p=1.0,
@@ -17,3 +18,15 @@ def generate_prompt(prompt):
         presence_penalty=0.0,
     )
     return response.choices[0].text.strip()
+
+
+def get_sentences(content):
+    pattern = r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s'
+    sentences = re.split(pattern, content)
+    return sentences
+
+
+def generate_sentences_prompts(content):
+    sentences = get_sentences(content)
+    prompts = [generate_prompt(sentence) for sentence in sentences]
+    return prompts
